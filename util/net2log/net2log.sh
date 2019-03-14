4da7b7a88f9299160f65c4785a9757e8c9d26a37
yasstake <yass@takibi.net>
2019-03-14 22:19:05 +0900

    update dockerfile

--- util/net2log/net2log.sh
+++ util/net2log/net2log.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 docker run --restart=always -v /tmp:/mexlog -t mmf /usr/bin/python3.7 /mmf/net2log.py /mexlog /mexlog/NET2LOG-FLG WORKA &
+
+sleep 120
+
 docker run --restart=always -v /tmp:/mexlog -t mmf /usr/bin/python3.7 /mmf/net2log.py /mexlog /mexlog/NET2LOG-FLG WORKB &
+docker run --restart=always -v /tmp:/mexlog -t mmf /usr/bin/python3.7 /mmf/upload /mexlog &
 
 
 
