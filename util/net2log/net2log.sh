e18dccaa88e0dfcc84d994cf0109279dcefbf2a5
yasstake <yass@takibi.net>
2019-03-15 21:25:32 +0900

    modyfy run script

--- util/net2log/net2log.sh
+++ util/net2log/net2log.sh
@@ -5,7 +5,7 @@ worker_name = $1
 /usr/bin/python3.7 /mmf/net2log.py /mexlog /mexlog/NET2LOG-FLG ${worker_name}
 result = $?
 
-/usr/bin/python3.7 /mmf/upload /mexlog
+/usr/bin/python3.7 /mmf/upload.py /mexlog
 
 if  [${result} == 0]; then
     echo "sleep 1H"
