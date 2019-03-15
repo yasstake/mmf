77b788d191f817449ee4ea474707d697814d1a15
yasstake <yass@takibi.net>
2019-03-15 22:22:40 +0900

    modyfy run script

--- util/net2log/docker_run.sh
+++ util/net2log/docker_run.sh
@@ -3,12 +3,12 @@
 docker rm $(docker ps -q -a)
 
 echo 'start daemon A'
-docker run --name WORKER-A -d -v /tmp:/mexlog --restart=always -t mmf bash /net2log.sh WORKER-A
+docker run --name WORKER-A -d -v /tmp:/mexlog --restart=always -t mmf bash /net2log.sh A
 
 sleep 10
 
 echo 'start daemon B'
-docker run --name WORKER-B -d -v /tmp:/mexlog --restart=always -t mmf bash /net2log.sh WORKER-B
+docker run --name WORKER-B -d -v /tmp:/mexlog --restart=always -t mmf bash /net2log.sh B
 
 
 
