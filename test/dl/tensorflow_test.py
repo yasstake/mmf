e85aea27bc8826714a2201575625c00a81b26c4e
yasstake <yass@takibi.net>
2019-03-23 15:25:21 +0900

    sample code to use TRrecords

--- test/dl/tensorflow_test.py
+++ test/dl/tensorflow_test.py
@@ -14,6 +14,48 @@ class TfTestCase(unittest.TestCase):
     def feature_bytes(self, a):
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[a]))
 
+
+
+    def test_save_single_value(self):
+        data = 1
+
+        output_file = '/tmp/onevalue.tfrecords'
+        pio = tf.python_io
+
+        writer = pio.TFRecordWriter(str(output_file))
+
+        record = tf.train.Example(features=tf.train.Features(feature={
+            'time' : self.feature_int64(data)
+            }))
+
+        writer.write(record.SerializeToString())
+
+        writer.close()
+
+
+    def test_load_single_value(self):
+        with tf.Session() as sess:
+            dataset = tf.data.TFRecordDataset('/tmp/onevalue.tfrecords')
+            dataset2 = dataset.map(TfTestCase.read_single_record)
+            iterator = dataset2.make_initializable_iterator()
+            next_dataset = iterator.get_next()
+            sess.run(iterator.initializer)
+            time = sess.run(next_dataset)
+            sess.close()
+
+        print('time->', time)
+
+    @staticmethod
+    def read_single_record(serialized):
+        features = tf.parse_single_example(
+            serialized,
+            features={
+                'time': tf.FixedLenFeature([], tf.int64)
+            })
+        time = features['time']
+
+        return time
+
     def test_save_file(self):
         board = PriceBoard()
 
@@ -26,13 +68,7 @@ class TfTestCase(unittest.TestCase):
 
         board.current_time = 1
         record = tf.train.Example(features=tf.train.Features(feature={
-            'time' : self.feature_int64(board.current_time)
-            }))
-
-        writer.write(record.SerializeToString())
-
-        board.current_time = 100
-        record = tf.train.Example(features=tf.train.Features(feature={
+            'buy': self.feature_bytes(board.buy_order.tobytes()),
             'time' : self.feature_int64(board.current_time)
             }))
 
@@ -42,46 +78,32 @@ class TfTestCase(unittest.TestCase):
 
 
     def test_load_file(self):
-        board = PriceBoard()
-
-        input_file = '/tmp/data.tfrecords'
-
-
-        data_set = tf.data.TFRecordDataset(input_file)
-
-        example = tf.train.Example()
-
-        data = example.ParseFromString(data_set)
-
-        print(data)
-
-
-    def test_load_file2(self):
         with tf.Session() as sess:
             dataset = tf.data.TFRecordDataset('/tmp/data.tfrecords')
-            dataset = dataset.map(TfTestCase.read_tfrecord)
-            iterator = dataset.make_initializable_iterator()
+            dataset2 = dataset.map(TfTestCase.read_tfrecord)
+            iterator = dataset2.make_initializable_iterator()
             next_dataset = iterator.get_next()
             sess.run(iterator.initializer)
+            time, buy = sess.run(next_dataset)
 
-            for i in range(2):
-                time =sess.run(next_dataset)
-                print('time->', time)
-            sess.close()
+        print('time->', time)
 
     @staticmethod
     def read_tfrecord(serialized):
+        buy = None
+        time = None
+
         features = tf.parse_single_example(
-        serialized,
-        features={
-        'time': tf.FixedLenFeature([], tf.int64)
-        })
-        time = tf.cast(features['time'], tf.int32)
+            serialized,
+            features={
+                'buy': tf.FixedLenFeature([], tf.string),
+                'time': tf.FixedLenFeature([], tf.int64)
+            })
 
-        return time
+        buy = features['buy']
+        time = features['time']
 
-
-
+        return buy, time
 
 
 if __name__ == '__main__':
