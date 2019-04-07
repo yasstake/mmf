410d2244a8ff5dab688a8e5b22696bf53d392f4e
yasstake <yass@takibi.net>
2019-04-08 00:10:37 +0900

    change best action definition

--- train.py
+++ train.py
@@ -1,12 +1,15 @@
 from dl.train import Train
+import sys
 
 
 if __name__ == "__main__":
+    train_pattern = sys.argv[1] + '/*.tfrecords'
+    test_pattern = sys.argv[2] +  '/*.tfrecords'
 
     train = Train()
     train.create_model()
 #    train.do_train('/tmp/2019/T/*.tfrecords', '/tmp/2019/T/*.tfrecords')
-    train.do_train('/tmp/TRAIN/*.tfrecords', '/tmp/2019/03/21/*.tfrecords')
+    train.do_train(train_pattern, test_pattern)
 
 #    train.do_train(('/tmp/2019/03/17/*.tfrecords', '/tmp/2019/03/18/*.tfrecords',
 #                    '/tmp/2019/03/19/*.tfrecords', '/tmp/2019/03/20/*.tfrecords'),
