b282091d38a7c2dd4308d4914523af1d4b4ca222
yass <yass@takibi.net>
2019-04-19 23:04:22 +0900

    Install script for Tensorflow

---
+++ util/install_gpu_step2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+nvidia-smi
+
+# Install development and runtime libraries (~4GB)
+sudo apt-get install --no-install-recommends \
+    cuda-10-0 \
+    libcudnn7=7.4.1.5-1+cuda10.0  \
+    libcudnn7-dev=7.4.1.5-1+cuda10.0
+
+
+# Install TensorRT. Requires that libcudnn7 is installed above.
+sudo apt-get update && \
+        sudo apt-get install nvinfer-runtime-trt-repo-ubuntu1804-5.0.2-ga-cuda10.0 \
+        && sudo apt-get update \
+        && sudo apt-get install -y --no-install-recommends libnvinfer-dev=5.0.2-1+cuda10.0
+
+
+pip3 install tensorflow-gpu==2.0.0a
