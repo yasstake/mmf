c96bfe78dd70b3061018af53b4c338cf03da7294
yasstake <yass@takibi.net>
2019-05-16 07:51:47 +0900

    To make use tensorboard in eager execution

--- env/log.py
+++ env/log.py
@@ -17,20 +17,20 @@ class Logger:
 
         # v2 mode
         self.summary = tf.contrib.summary.create_file_writer(log_dir, flush_millis=10000)
-        self.total_timesteps = tf.train.create_global_step()
+        #self.total_timesteps = tf.train.create_global_step()
         #  tf.contrib.summary.record_summaries_every_n_global_steps
 
     def log_loss(self, loss, episode):
         with self.summary.as_default(), tf.contrib.summary.always_record_summaries():
-            self.total_timesteps.assign_add(episode)
+            #self.total_timesteps.assign_add(episode)
             tf.contrib.summary.scalar('loss', loss)
 
 
     def log_episode(self, episode, loss, reward, total_reward):
         with self.summary.as_default(), tf.contrib.summary.always_record_summaries():
-            tf.summary.scalar('loss', reward, step=episode)
-            tf.summary.scalar('reward', reward, step=episode)
-            tf.summary.scalar('total reward', total_reward, step=episode)
+            tf.contrib.summary.scalar('loss', reward, step=episode)
+            tf.contrib.summary.scalar('reward', reward, step=episode)
+            tf.contrib.summary.scalar('total reward', total_reward, step=episode)
 
 
 if __name__ == '__main__':
